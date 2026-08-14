115d01ccde473d43e566e603963171c047ab3409
svarna098 <svarna098@gmail.com>
2026-08-15 00:15:03 +0530

    Update fifo_package.sv

--- fifo_uvm/src/testbench/fifo_package.sv
+++ fifo_uvm/src/testbench/fifo_package.sv
@@ -11,6 +11,7 @@ package fifo_package;
 	`include "fifo_output_monitor.sv"
 	`include "fifo_out_agent.sv"
 	`include "fifo_scoreboard.sv"
+        `include "fifo_subscriber.sv"
 	`include "fifo_environment.sv"
 	`include "fifo_sequence.sv"
 	`include "fifo_test.sv"
